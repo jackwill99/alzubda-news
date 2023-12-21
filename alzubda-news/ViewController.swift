b041bf801cd26f9298e1ddf7c0561fd8ccb1cb57
Jack Will <65641460+jackwill99@users.noreply.github.com>
2023-12-21 10:52:29 +0630

    Initial Commit

---
+++ alzubda-news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alzubda-news
+//
+//  Created by Tensor Lab on 12/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
